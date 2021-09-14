e13f0d7d571f3ce2c4352ac0760b3611507e2a6e
Bruno Santos <Bruno.SSantos.EXT@thomsonreuters.com>
2021-09-14 08:55:49 -0300

    Linked list model

---
+++ TestProject/TestProject/Util/LinkedList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.Util
+{
+    public class LinkedListNode<T>
+    {
+        public T Data { get; set; }
+
+        private LinkedListNode<T> Next;
+
+        public LinkedListNode<T> Add(T value) 
+        { 
+            this.Next = new LinkedListNode<T>
+            {
+                Data = value,
+                Next = null
+            };
+            
+            return this.Next;
+        }
+
+        public void Add(LinkedListNode<T> node) 
+        { 
+            this.Next = node;
+        }
+
+        public LinkedListNode<T> GetNext() => this.Next;
+    }
+
+    public static class LinkedListHelper
+    {
+        /// <summary>
+        /// Initialize a new list and returns its initial node
+        /// </summary>
+        /// <param name="values"></param>
+        /// <param name="loopNode">If valid value, last node will point for the node in this index </param>
+        /// <returns></returns>
+        public static LinkedListNode<T> InitializeList<T>(T[] values, int loopIndex = -1)
+        {
+            if(values.Length == 0) throw new ArgumentException();
+            
+            var initialNode = new LinkedListNode<T>();
+            initialNode.Data = values[0];
+
+            var listBuilder = initialNode;
+
+            // Build list
+            for (int i = 1; i < values.Length; i++)
+            {
+                listBuilder = listBuilder.Add(values[i]);
+            }
+
+            // Insert a loop on last node
+            if(loopIndex >= 0 && loopIndex < values.Length)
+            {
+                var loopNode = initialNode;
+
+                for(int i = 0; i < loopIndex; i++)
+                {
+                    loopNode = loopNode.GetNext();
+                }
+
+                listBuilder.Add(loopNode);
+            }
+
+            return initialNode;
+        }
+    }
+
+}
